2d00ddd01f6a18d7903c4b009dd2642e70ceb3c0
Afsana <abdullayevaynur527@gmail.com>
2024-04-01 16:19:50 +0400

    js

--- assets/js/index.js
+++ assets/js/index.js
@@ -28,7 +28,7 @@ function repeatProduct(products) {
   products.forEach(function (product) {
     // console.log(product.images[0])
     // for(let i=0;i<=4;i++){
-      cards.innerHTML += `<div class="card-item">
+      cards.innerHTML += `<a href="./productDetail.html?id=${product.id}" class="card-item" onclick="productDetails()">
       <div class="card-item--body display__between">
         <div class="cardItem-header">
           <div class="card-percantage">
@@ -50,7 +50,7 @@ function repeatProduct(products) {
         <p class="common-poppins  card--cartIcon">Add To Cart</p>
       </div>
       <div class="card-item--minifooter">
-        <p class="poppins-font500">${product.title}</p>
+        <p class="poppins-font500 card-title">${product.title}</p>
         <p class="poppins-font500 wishList-miniFoot">$${product.price}<span class="wishList-miniFoot--subtitle">$${product.discount}</span></p>
         <div class="star-icons">
           <div class="star-icon--div">
@@ -63,87 +63,12 @@ function repeatProduct(products) {
           <span class="poppins-font600 count">(${product.stock})</span>
         </div>
       </div>              
-    </div>`;
+    </a>`;
     // }
     
   });
 }
   
-
-
-// Swiper'ı başlatın
-
-// function chunkArray(arr, chunkSize) {
-//   const chunks = [];
-//   for (let i = 0; i < arr.length; i += chunkSize) {
-//     chunks.push(arr.slice(i, i + chunkSize));
-//   }
-//   return chunks;
-// }
-
-
-// function repeatProduct(products) {
-//   cards.innerHTML = ''; // cards içeriğini temizle
-
-//   // Slayt sayısını ve her bir slaytta gösterilecek ürün sayısını belirleyin
-//   const slidesPerView = 4; // Her slaytta gösterilecek ürün sayısı
-//   const totalSlides = Math.ceil(products.length / slidesPerView); // Toplam slayt sayısı
-
-//   // Slaytları oluşturun
-//   for (let i = 0; i < totalSlides; i++) {
-//     const slideProducts = products.slice(i * slidesPerView, (i + 1) * slidesPerView);
-
-//     const slideHTML = slideProducts.map(product => `
-//       <div class="swiper-slide">
-//         <div class="card-item">
-//           <div class="card-item--body display__between">
-//             <div class="cardItem-header">
-//               <div class="card-percantage">
-//                 <!-- <p>asdcvb</p> -->
-//               </div>
-//               <div class="card-Icons">
-//                 <div class="card-Icon display__com">
-//                   <i class="fa-regular fa-heart"></i>
-//                 </div>
-//                 <div class="card-Icon display__com">
-//                   <i class="fa-regular fa-eye"></i>
-//                 </div>
-//               </div>
-//             </div>
-//             <img src="${product.images[0]}" alt="cart image" />
-//           </div>
-//           <div class="card-item--footer display__com">
-//             <i class="fa-solid fa-cart-shopping"></i>
-//             <p class="common-poppins  card--cartIcon">Add To Cart</p>
-//           </div>
-//           <div class="card-item--minifooter">
-//             <p class="poppins-font500">${product.title}</p>
-//             <p class="poppins-font500 wishList-miniFoot">$${product.price}<span class="wishList-miniFoot--subtitle">$${product.discount}</span></p>
-//             <div class="star-icons">
-//               <div class="star-icon--div">
-//                 <i class="fa-solid fa-star"></i>
-//                 <i class="fa-solid fa-star"></i>
-//                 <i class="fa-solid fa-star"></i>
-//                 <i class="fa-solid fa-star"></i>
-//                 <i class="fa-solid fa-star"></i>
-//               </div>
-//               <span class="poppins-font600 count">(${product.stock})</span>
-//             </div>
-//           </div> 
-//         </div>
-//       </div>
-//     `).join('');
-
-//     // Oluşturulan slayt HTML'ini cards içeriğine ekleyin
-//     cards.innerHTML += `
-//       <div class="swiper-container card-slider">
-//         <div class="swiper-wrapper">
-//           ${slideHTML}
-//         </div>
-//         <div class="swiper-pagination"></div>
-//       </div>
-//     `;
-//   }
 
 //   // Swiper'ı başlatın
 //   const sliders = document.querySelectorAll('.card-slider');
@@ -162,118 +87,6 @@ function repeatProduct(products) {
 // }
 
 
-
-
-
-
-
-// function repeatProduct(products) {
-//     cards.innerHTML += " ";
-//     products.forEach(function (product) {
-//       // console.log(product.images[0])
-//       cards.innerHTML += `<div class="card-item">
-//       <div class="card-item--body display__between">
-//         <div class="cardItem-header">
-//           <div class="card-percantage">
-//             <!-- <p>asdcvb</p> -->
-//           </div>
-//           <div class="card-Icons">
-//             <div class="card-Icon display__com">
-//             <i class="fa-regular fa-heart"></i>
-//           </div>
-//           <div class="card-Icon display__com">
-//             <i class="fa-regular fa-eye"></i>
-//             </div>
-//           </div>
-//         </div>
-//         <img src=${product.images[0]} alt="cart image" />
-//       </div>
-//       <div class="card-item--footer display__com">
-//         <i class="fa-solid fa-cart-shopping"></i>
-//         <p class="common-poppins  card--cartIcon">Add To Cart</p>
-//       </div>
-//       <div class="card-item--minifooter">
-//         <p class="poppins-font500">${product.title}</p>
-//         <p class="poppins-font500 wishList-miniFoot">$${product.price}<span class="wishList-miniFoot--subtitle">$${product.discount}</span></p>
-//         <div class="star-icons">
-//           <div class="star-icon--div">
-//             <i class="fa-solid fa-star"></i>
-//             <i class="fa-solid fa-star"></i>
-//             <i class="fa-solid fa-star"></i>
-//             <i class="fa-solid fa-star"></i>
-//             <i class="fa-solid fa-star"></i>
-//           </div>
-//           <span class="poppins-font600 count">(${product.stock})</span>
-//         </div>
-//       </div>              
-//     </div>`;
-//     });
-
-//   const chunkedProducts = chunkArray(products, 4); // Ürünleri 4'lü gruplara ayır
-
-//   chunkedProducts.forEach((chunk, index) => {
-//     cards.innerHTML += `<div class="swiper-container card-slider" id="slider${index}">
-//       <div class="swiper-wrapper">
-//         ${chunk.map(product => `<div class="swiper-slide">
-//           <div class="card-item">
-//             <!-- card-item content -->
-//           </div>
-//         </div>`).join('')}  
-//       </div>
-//       <div class="swiper-pagination"></div>
-//     </div>`;
-    
-//     new Swiper(`#slider${index}`, {
-//       slidesPerView: 1,
-//       loop: false,
-//       grabCursor: true,
-//       pagination: {
-//         el: `.swiper-pagination`,
-//         clickable: true,
-//       },
-//     });
-//   });
-// }
-
-
-
-
-
-// var swiper = new Swiper(".card-item", {
-//   slidesPerView: 1,
-//   loop: true,
-//   centerSlide: true,
-//   fade: true,
-//   grabCursor: true,
-//   spaceBetween: 30,
-//   pagination: {
-//     el: ".swiper-pagination",
-//     clickable: true,
-//   },
-//   navigation: {
-//     nextEl: ".fa-arrow-right",
-//     prevEl: ".fa-arrow-left",
-//   },
-// });
-
-
-// var swiper = new Swiper(".card-items", {
-//   slidesPerView: 1,
-//   loop: true,
-//   centerSlide:'true',
-//   fade: 'true',
-//   grabCursor: 'true',
-//   grid: {
-//     rows: 2,
-//   },
-//   spaceBetween: 70,
-//   pagination: {
-//     el: ".swiper-pagination",
-//     clickable: true,
-//   },
-//   navigation: {
-//     nextEl: ".fa-arrow-right",
-//     prevEl: ".fa-arrow-left",
-//   },
-// });
-// console.log(swiper)
+function productDetails(){
+  window.location.href="productDetail.html"
+}
